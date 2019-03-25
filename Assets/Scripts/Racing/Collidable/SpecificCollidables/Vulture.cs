fab768292b4354c4e972fa0f500bc5f9214e1bc8
Grace Rarer <gracerarer@gmail.com>
2019-03-25 19:50:23 -0400

    vulture rotation pivot
    vultures now orbit around in a circle

--- Assets/Scripts/Racing/Collidable/SpecificCollidables/Vulture.cs
+++ Assets/Scripts/Racing/Collidable/SpecificCollidables/Vulture.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 
 namespace Racing.Collidables {
+
+
+	
+
 	public class Vulture : Instantaneous {
 
 		private const float BOOST_STRENGTH = 0.5f;
@@ -10,7 +14,6 @@ namespace Racing.Collidables {
 			Rigidbody rb = glider.GetComponent<Rigidbody>();
 
 			rb.velocity *= Vulture.BOOST_STRENGTH;
-
 		}
 	}
 }
