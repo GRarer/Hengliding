5f17c1fe8ac563b096accfd1100efefe04126c45
Cinnamon18 <alice.ava.moh@gmail.com>
2019-03-29 16:03:16 -0400

    wind lines

--- Assets/Scripts/Racing/WindLine.cs
+++ Assets/Scripts/Racing/WindLine.cs
@@ -7,14 +7,17 @@ public class WindLine : MonoBehaviour {
 
 	// Start is called before the first frame update
 	void Start() {
-		
+
 	}
 
 	// Update is called once per frame
 	void Update() {
 		transform.position = transform.position + new Vector3(0, 10, 0) * Time.deltaTime;
-		if(Random.Range(0.0f, 1.0f) < 0.002) {
+		if (Random.Range(0.0f, 1.0f) < 0.001) {
 			GetComponentInChildren<Animator>().SetTrigger("Twirl");
+		}
+		if (Random.Range(0.0f, 1.0f) < 0.002) {
+			GetComponentInChildren<Animator>().SetTrigger("Wiggle");
 		}
 	}
 }
