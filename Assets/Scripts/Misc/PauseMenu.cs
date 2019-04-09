e16f3f16a4201130a80aceab0b200d115ad42792
Cinnamon18 <alice.ava.moh@gmail.com>
2019-04-08 20:42:57 -0400

    pause menu

---
+++ Assets/Scripts/Misc/PauseMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+	// Update is called once per frame
+
+	public GameObject pauseMenu;
+
+	void Update() {
+		if(Input.GetButtonDown("Cancel")) {
+			if(pauseMenu.activeInHierarchy) {
+				hidePauseMenu();
+			} else {
+				showPauseMenu();
+			}
+		}
+	}
+
+	public void showPauseMenu() {
+		pauseMenu.SetActive(true);
+	}
+
+	public void hidePauseMenu() {
+		pauseMenu.SetActive(false);
+	}
+
+	public void mainMenu() {
+		SceneManager.LoadScene("Title");
+	}
+}
