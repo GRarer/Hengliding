11d879a86f5c818415431911534c42f3949d7a58
unknown <Triplett.Seth@gmail.com>
2019-04-08 22:11:35 -0400

    Added sound management system with example placeholders.

---
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public enum SFX {
+        sfx1,
+        sfx2,
+    }
+
+    [SerializeField] AudioSource bgm;
+    [SerializeField] AudioSource[] sfxArray;
+
+    void Start() {
+        PlayBGM();
+    }
+
+    public void PlayBGM() {
+        bgm.Play();
+    }
+
+    public void PauseBGM() {
+        bgm.Pause();
+    }
+
+    public void StopBGM() {
+        bgm.Stop();
+    }
+
+    public void UnPauseBGM() {
+        bgm.UnPause();
+    }
+
+    public void Playsfx(SFX sfxEnum) {
+        if (sfxArray[(int) sfxEnum].isPlaying) {
+            sfxArray[(int) sfxEnum].Stop();
+        }
+        sfxArray[(int) sfxEnum].Play();
+    }
+
+    public void StopAllsfx() {
+        for (int i = 0; i < sfxArray.Length; i++) {
+            sfxArray[i].Stop();
+        }
+    }
+
+}
